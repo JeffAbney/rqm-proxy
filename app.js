42ef93848797cea393039d3f5b4b70870e318f26
JeffAbney <jeffmabney@gmail.com>
2019-09-24 09:37:19 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send("PROXY SERVER UP IN HERE")
+  // request(
+  //     { url: 'https://quotesondesign.com/wp-json/wp/v2/posts?filter[orderby]=rand' },
+  //     (error, response, body) => {
+  //         if (error || response.statusCode !== 200) {
+  //             return res.status(500).json({ type: 'error', message: err.message });
+  //         }
+  //         console.log("here's body", body)
+  //         res.send(body);
+  //     }
+  // )
+});
+
+module.exports = app;
