de75a78ab668bacbbcf33c598c9195d86b758ff9
JeffAbney <jeffmabney@gmail.com>
2019-09-25 11:29:43 -0500

    add res.header in use

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 var express = require('express');
 const app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Authorization, Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 app.get('/', (req, res) => {
   res.send("PROXY SERVER UP IN HERE!!!")
   // request(
