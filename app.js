956322728ed8e8cb798f84e36710805ca130d49e
JeffAbney <jeffmabney@gmail.com>
2019-09-25 11:34:32 -0500

    uncomment proxy request

--- app.js
+++ app.js
@@ -11,16 +11,16 @@ app.use(function(req, res, next) {
 
 app.get('/', (req, res) => {
   res.send("PROXY SERVER UP IN HERE!!!")
-  // request(
-  //     { url: 'https://quotesondesign.com/wp-json/wp/v2/posts?filter[orderby]=rand' },
-  //     (error, response, body) => {
-  //         if (error || response.statusCode !== 200) {
-  //             return res.status(500).json({ type: 'error', message: err.message });
-  //         }
-  //         console.log("here's body", body)
-  //         res.send(body);
-  //     }
-  // )
+  request(
+      { url: 'https://quotesondesign.com/wp-json/wp/v2/posts?filter[orderby]=rand' },
+      (error, response, body) => {
+          if (error || response.statusCode !== 200) {
+              return res.status(500).json({ type: 'error', message: err.message });
+          }
+          console.log("here's body", body)
+          res.send(body);
+      }
+  )
 });
 
 module.exports = app;
